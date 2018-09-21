936168037fa28214bcbbdf6743a585841231a970
Carlo Bramini <braminicarlo@live.com>
2018-09-22 00:00:27 +0200

    completed some csv data readers

--- CountryDataForGis.Test/UnitTest1.cs
+++ CountryDataForGis.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -16,23 +17,197 @@ namespace CountryDataForGis.Test
 
 
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Reader_GitHubUser_Datasets_GetDataBy_ISO3166_1_Alpha_3()
         {
             // arrange 
             DataService dataService = new DataService(this.FolderPath_CountryData);
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
 
 
             // assert 
             Dictionary<string,string> result = dataService.Reader_GitHubUser_Datasets.GetDataBy_ISO3166_1_Alpha_3("ITA");
+            KeyValuePair<string, string> kvp_Dial = result.Single(x => x.Key == "Dial");
+            KeyValuePair<string, string> kvp_Languages = result.Single(x => x.Key == "Languages");
+           
 
-            KeyValuePair<string, string> kvp = result.Single(x => x.Key == "ITU");
-            string key = kvp.Key.ToString();
-            string value = kvp.Value.ToString();
 
             // act
+            Assert.IsTrue(kvp_Dial.Value == "39");
+            Assert.IsTrue(kvp_Languages.Value == "it-IT,de-IT,fr-IT,sc,ca,co,sl");
 
 
+            stopwatch.Stop();
+            string timeElpsed = stopwatch.Elapsed.ToString();
+        }
+
+        [TestMethod]
+        public void Test_Reader_GitHubUser_Datasets_GetAllData()
+        {
+            // arrange 
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            DataService dataService = new DataService(this.FolderPath_CountryData);
+
+
+            // assert 
+            List<Dictionary<string, string>> result = dataService.Reader_GitHubUser_Datasets.GetAllData();
+            Dictionary<string, string> result_filtered = new Dictionary<string, string>();
+
+            foreach (Dictionary<string, string> dict in result)
+            {
+                string testKey = "ISO3166-1-Alpha-3";
+                string testValue = "ITA";
+
+                KeyValuePair<string,string> kvpRes = dict.SingleOrDefault(x => x.Key == testKey && x.Value == testValue);
+                if (kvpRes.Key == testKey && kvpRes.Value == testValue)
+                {
+                    result_filtered = dict;
+                    continue;
+                }
+            }
+
+            KeyValuePair<string, string> kvp_Dial = result_filtered.Single(x => x.Key == "Dial");
+            KeyValuePair<string, string> kvp_Languages = result_filtered.Single(x => x.Key == "Languages");
+
+
+
+            // act
+            Assert.IsTrue(kvp_Dial.Value == "39");
+            Assert.IsTrue(kvp_Languages.Value == "it-IT,de-IT,fr-IT,sc,ca,co,sl");
+
+
+
+            stopwatch.Stop();
+            string timeElpsed = stopwatch.Elapsed.ToString();
 
         }
+
+
+
+
+        [TestMethod]
+        public void Test_Reader_GitHubUser_Umpirsky_Country_GetCountryDataAll()
+        {
+            //*******************
+            // arrange 
+            //*******************
+            DataService dataService = new DataService(this.FolderPath_CountryData);
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //*******************
+            // act 
+            //*******************
+            dataService.Reader_GitHubUser_Umpirsky_Country.SetLocalFolder("en_GB");
+            Dictionary<string, string> result_en_GB = dataService.Reader_GitHubUser_Umpirsky_Country.GetCountryDataAll();
+
+            dataService.Reader_GitHubUser_Umpirsky_Country.SetLocalFolder("it_IT");
+            Dictionary<string, string> result_it_IT = dataService.Reader_GitHubUser_Umpirsky_Country.GetCountryDataAll();
+
+            bool isPresentInenGB = result_en_GB.Any(x => x.Key == "FR" && x.Value == "France");
+            bool isPresentInitIT = result_it_IT.Any(x => x.Key == "FR" && x.Value == "Francia");
+
+            //check if a single iso_3166_1_alpha_2 exist and get the value by langauge
+            dataService.Reader_GitHubUser_Umpirsky_Country.SetLocalFolder("es_ES");
+            KeyValuePair<string, string> kvp_esES = dataService.Reader_GitHubUser_Umpirsky_Country.GetValueBy_ISO_3166_1_alpha_2("US");
+
+
+
+            //*******************
+            // assert
+            //*******************
+            Assert.IsTrue(isPresentInenGB);
+            Assert.IsTrue(isPresentInitIT);
+            Assert.IsTrue(kvp_esES.Value == "Estados Unidos");
+
+            stopwatch.Stop();
+            string timeElpsed = stopwatch.Elapsed.ToString();
+        }
+
+        [TestMethod]
+        public void Test_Reader_GitHubUser_Umpirsky_Language_GetLanguageDataAll()
+        {
+            //*******************
+            // arrange 
+            //*******************
+            DataService dataService = new DataService(this.FolderPath_CountryData);
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //*******************
+            // act 
+            //*******************
+            dataService.Reader_GitHubUser_Umpirsky_Language.SetLocalFolder("en_GB");
+            Dictionary<string, string> result_en_GB = dataService.Reader_GitHubUser_Umpirsky_Language.GetLanguageDataAll();
+
+            dataService.Reader_GitHubUser_Umpirsky_Language.SetLocalFolder("it_IT");
+            Dictionary<string, string> result_it_IT = dataService.Reader_GitHubUser_Umpirsky_Language.GetLanguageDataAll();
+
+            bool isPresentInenGB = result_en_GB.Any(x => x.Key == "fr" && x.Value == "French");
+            bool isPresentInitIT = result_it_IT.Any(x => x.Key == "fr" && x.Value == "francese");
+
+            //check if a single iso_3166_1_alpha_2 exist and get the value by langauge
+            dataService.Reader_GitHubUser_Umpirsky_Language.SetLocalFolder("es_ES");
+            KeyValuePair<string, string> kvp_esES = dataService.Reader_GitHubUser_Umpirsky_Language.GetValueBy_LanguageCode("en_US");
+
+
+
+            //*******************
+            // assert
+            //*******************
+            Assert.IsTrue(isPresentInenGB);
+            Assert.IsTrue(isPresentInitIT);
+            Assert.IsTrue(kvp_esES.Value == "inglés estadounidense");
+
+            stopwatch.Stop();
+            string timeElpsed = stopwatch.Elapsed.ToString();
+        }
+
+        [TestMethod]
+        public void Test_Reader_GitHubUser_Umpirsky_Locale_GetLocaleDataAll()
+        {
+            //*******************
+            // arrange 
+            //*******************
+            DataService dataService = new DataService(this.FolderPath_CountryData);
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //*******************
+            // act 
+            //*******************
+            dataService.Reader_GitHubUser_Umpirsky_Locale.SetLocalFolder("en_GB");
+            Dictionary<string, string> result_en_GB = dataService.Reader_GitHubUser_Umpirsky_Locale.GetLocaleDataAll();
+
+            dataService.Reader_GitHubUser_Umpirsky_Locale.SetLocalFolder("it_IT");
+            Dictionary<string, string> result_it_IT = dataService.Reader_GitHubUser_Umpirsky_Locale.GetLocaleDataAll();
+
+            bool isPresentInenGB = result_en_GB.Any(x => x.Key == "fr_FR" && x.Value == "French (France)");
+            bool isPresentInitIT = result_it_IT.Any(x => x.Key == "fr_FR" && x.Value == "francese (Francia)");
+
+            //check if a single iso_3166_1_alpha_2 exist and get the value by langauge
+            dataService.Reader_GitHubUser_Umpirsky_Locale.SetLocalFolder("es_ES");
+            KeyValuePair<string, string> kvp_esES = dataService.Reader_GitHubUser_Umpirsky_Locale.GetValueBy_LocaleCode("en_US");
+
+
+
+            //*******************
+            // assert
+            //*******************
+            Assert.IsTrue(isPresentInenGB);
+            Assert.IsTrue(isPresentInitIT);
+            Assert.IsTrue(kvp_esES.Value == "inglés (Estados Unidos)");
+
+            stopwatch.Stop();
+            string timeElpsed = stopwatch.Elapsed.ToString();
+        }
+
+
     }
 }
--- CountryDataForGis/Program.cs
+++ CountryDataForGis/Program.cs
@@ -26,9 +26,14 @@ namespace CountryDataForGis
 
         //Data readers
         public Reader_GitHubUser_Datasets Reader_GitHubUser_Datasets { get; set; }
+        public Reader_GitHubUser_Umpirsky_Country Reader_GitHubUser_Umpirsky_Country { get; set; }
+        public Reader_GitHubUser_Umpirsky_Language Reader_GitHubUser_Umpirsky_Language { get; set; }
+        public Reader_GitHubUser_Umpirsky_Locale Reader_GitHubUser_Umpirsky_Locale { get; set; }
 
 
-
+        //**********************
+        //CONSTRUCTOR
+        //**********************
         public DataService(string folderPath_CountryData)
         {
             BasicHelpers.CheckIfFolderPathExits(folderPath_CountryData);
@@ -37,7 +42,10 @@ namespace CountryDataForGis
 
 
             //Data readers
-            this.Reader_GitHubUser_Datasets = new Reader_GitHubUser_Datasets( $@"{this._FolderPath_CountryData}FromGitHub\datasets\country-codes\country-codes.csv" );
+            this.Reader_GitHubUser_Datasets = new Reader_GitHubUser_Datasets($@"{this._FolderPath_CountryData}FromGitHub\datasets\country-codes\country-codes.csv");
+            this.Reader_GitHubUser_Umpirsky_Country = new Reader_GitHubUser_Umpirsky_Country($@"{this._FolderPath_CountryData}FromGitHub\umpirsky\country-list\", "it_IT");
+            this.Reader_GitHubUser_Umpirsky_Language = new Reader_GitHubUser_Umpirsky_Language($@"{this._FolderPath_CountryData}FromGitHub\umpirsky\language-list\", "it_IT");
+            this.Reader_GitHubUser_Umpirsky_Locale = new Reader_GitHubUser_Umpirsky_Locale($@"{this._FolderPath_CountryData}FromGitHub\umpirsky\locale-list\", "it_IT");
         }
     }
 
@@ -45,14 +53,18 @@ namespace CountryDataForGis
 
 
 
-    public  class Reader_GitHubUser_Datasets
+    public class Reader_GitHubUser_Datasets
     {
         private string _FilePath_csv_countryCodes { get; set; }
 
+        //**********************
+        //CONSTRUCTOR
+        //**********************
         public Reader_GitHubUser_Datasets(string filePath_csv_countryCodes)
         {
             this._FilePath_csv_countryCodes = filePath_csv_countryCodes;
         }
+
 
 
         public Dictionary<string, string> GetDataBy_ISO3166_1_Alpha_3(string ISO3166_1_Alpha_3)
@@ -73,7 +85,7 @@ namespace CountryDataForGis
                     if (isThisFirstHeaderRow)
                     {
                         string lineHeader = reader.ReadLine();
-                        arrHeaders = this._SplitCSV(lineHeader); //lineHeader.Split(',');
+                        arrHeaders = BasicHelpers._SplitCSV(lineHeader); //lineHeader.Split(',');
 
                         for (int i = 0; i < arrHeaders.Length; i++)
                         {
@@ -82,7 +94,7 @@ namespace CountryDataForGis
                                 indexISO3166_1_Alpha_3 = i;
                                 break;
                             }
-                            if (i == arrHeaders.Length-1)
+                            if (i == arrHeaders.Length - 1)
                             {
                                 throw new Exception($@"ZR in GetDataBy_ISO3166_1_Alpha_3(string ISO3166_1_Alpha_3), there is no header with name ISO3166_1_Alpha_3");
                             }
@@ -94,14 +106,15 @@ namespace CountryDataForGis
 
                     //Read rows below header
                     var line = reader.ReadLine();
-                    string[] values = this._SplitCSV(line);//line.Split(',');
+                    string[] values = BasicHelpers._SplitCSV(line);//line.Split(',');
 
                     //Save Header as key and row value as value in dictionary
-                    if(values[indexISO3166_1_Alpha_3]== ISO3166_1_Alpha_3)
+                    if (values[indexISO3166_1_Alpha_3] == ISO3166_1_Alpha_3)
                     {
                         for (int i = 0; i < values.Length; i++)
                         {
-                            resultDictionary.Add(arrHeaders[i], values[i]);
+                            string valueCleanedStr = BasicHelpers._TrimQuotesFromString(values[i]);
+                            resultDictionary.Add(arrHeaders[i], valueCleanedStr);
                         }
                     }
 
@@ -113,9 +126,344 @@ namespace CountryDataForGis
 
         }
 
+        public List<Dictionary<string, string>> GetAllData()
+        {
+            List<Dictionary<string, string>> listResultDictionary = new List<Dictionary<string, string>>();
+
+            using (var reader = new StreamReader(this._FilePath_csv_countryCodes))
+            {
+
+                bool isThisFirstHeaderRow = true;
+                string[] arrHeaders = new string[] { };
+
+                while (!reader.EndOfStream)
+                {
+                    //Read header
+                    if (isThisFirstHeaderRow)
+                    {
+                        var lineHeader = reader.ReadLine();
+                        arrHeaders = BasicHelpers._SplitCSV(lineHeader);//lineHeader.Split(',');
+                        isThisFirstHeaderRow = false;
+                        continue;
+                    }
+
+                    //Read rows below header
+                    var line = reader.ReadLine();
+                    string[] values = BasicHelpers._SplitCSV(line); //line.Split(',');
+
+                    //Save Header as key and row value as value in dictionary
+                    Dictionary<string, string> dictRowKeyValue = new Dictionary<string, string>();
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        string valueCleanedStr = BasicHelpers._TrimQuotesFromString(values[i]);
+                        dictRowKeyValue.Add(arrHeaders[i], valueCleanedStr);
+                    }
+                    listResultDictionary.Add(dictRowKeyValue);
+                }
+            }
+
+            return listResultDictionary;
+
+        }
 
 
-        private string[] _SplitCSV(string input)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public class Reader_GitHubUser_Umpirsky_Country
+    {
+        private string _FilePath_dataFolder { get; set; }
+        private string _LocaleFolder { get; set; }
+
+        private string _GetFileDataFrom_csv { get { return $@"{this._FilePath_dataFolder}{this._LocaleFolder}\country.csv"; } }
+
+        //**********************
+        //CONSTRUCTOR
+        //**********************
+        public Reader_GitHubUser_Umpirsky_Country(string filePath_dataFolder, string localeFolder)
+        {
+            this._FilePath_dataFolder = filePath_dataFolder;
+            this._LocaleFolder = localeFolder;
+        }
+
+        public void SetLocalFolder(string localFolderName)
+        {
+            this._LocaleFolder = localFolderName;
+        }
+
+        public Dictionary<string, string> GetCountryDataAll()
+        {
+            Dictionary<string, string> keyValuePairData = new Dictionary<string, string>();
+
+            using (var reader = new StreamReader(this._GetFileDataFrom_csv))
+            {
+
+                bool isThisFirstHeaderRow = true;
+
+                while (!reader.EndOfStream)
+                {
+                    //Read header
+                    if (isThisFirstHeaderRow)
+                    {
+                        isThisFirstHeaderRow = false;
+                        continue;
+                    }
+
+                    //Read rows below header
+                    var line = reader.ReadLine();
+                    string[] key_value = BasicHelpers._SplitCSV(line);//line.Split(',');
+
+                    //Save Header as key and row value as value in dictionary
+                    keyValuePairData.Add(BasicHelpers._TrimQuotesFromString(key_value[0]), BasicHelpers._TrimQuotesFromString(key_value[1]));
+
+                }
+            }
+
+            return keyValuePairData;
+        }
+
+        public KeyValuePair<string, string> GetValueBy_ISO_3166_1_alpha_2(string iso_3166_1_alpha_2)
+        {
+            using (var reader = new StreamReader(this._GetFileDataFrom_csv))
+            {
+
+                bool isThisFirstHeaderRow = true;
+
+                while (!reader.EndOfStream)
+                {
+                    //Read header
+                    if (isThisFirstHeaderRow)
+                    {
+                        var line_header = reader.ReadLine();
+                        isThisFirstHeaderRow = false;
+                        continue;
+                    }
+
+                    //Read rows below header
+                    var line = reader.ReadLine();
+                    string[] key_value = BasicHelpers._SplitCSV(line);//line.Split(',');
+
+                    //Check if iso3166 1 alpha 2 matches
+                    if(key_value[0] == iso_3166_1_alpha_2)
+                    {
+                        var kvp = new KeyValuePair<string, string>(BasicHelpers._TrimQuotesFromString(key_value[0]), BasicHelpers._TrimQuotesFromString(key_value[1]));
+                        return kvp;
+                    }
+
+                }
+            }
+
+            throw new Exception($@"ZR in GetValueBy_ISO_3166_1_alpha_2(string iso_3166_1_alpha_2): could not find iso_3166_1_alpha_2 with key of {iso_3166_1_alpha_2}");
+
+        }
+
+    }
+
+    public class Reader_GitHubUser_Umpirsky_Language
+    {
+        private string _FilePath_dataFolder { get; set; }
+        private string _LocaleFolder { get; set; }
+
+        private string _GetFileDataFrom_csv { get { return $@"{this._FilePath_dataFolder}{this._LocaleFolder}\language.csv"; } }
+
+        //**********************
+        //CONSTRUCTOR
+        //**********************
+        public Reader_GitHubUser_Umpirsky_Language(string filePath_dataFolder, string localeFolder)
+        {
+            this._FilePath_dataFolder = filePath_dataFolder;
+            this._LocaleFolder = localeFolder;
+        }
+
+        public void SetLocalFolder(string localFolderName)
+        {
+            this._LocaleFolder = localFolderName;
+        }
+
+        public Dictionary<string, string> GetLanguageDataAll()
+        {
+            Dictionary<string, string> keyValuePairData = new Dictionary<string, string>();
+
+            using (var reader = new StreamReader(this._GetFileDataFrom_csv))
+            {
+
+                bool isThisFirstHeaderRow = true;
+
+                while (!reader.EndOfStream)
+                {
+                    //Read header
+                    if (isThisFirstHeaderRow)
+                    {
+                        var line_header = reader.ReadLine();
+                        isThisFirstHeaderRow = false;
+                        continue;
+                    }
+
+                    //Read rows below header
+                    var line = reader.ReadLine();
+                    string[] key_value = BasicHelpers._SplitCSV(line);//line.Split(',');
+
+                    //Save Header as key and row value as value in dictionary
+                    keyValuePairData.Add(BasicHelpers._TrimQuotesFromString(key_value[0]), BasicHelpers._TrimQuotesFromString(key_value[1]));
+
+                }
+            }
+
+            return keyValuePairData;
+        }
+
+        public KeyValuePair<string, string> GetValueBy_LanguageCode(string languageCode)
+        {
+            using (var reader = new StreamReader(this._GetFileDataFrom_csv))
+            {
+
+                bool isThisFirstHeaderRow = true;
+
+                while (!reader.EndOfStream)
+                {
+                    //Read header
+                    if (isThisFirstHeaderRow)
+                    {
+                        isThisFirstHeaderRow = false;
+                        continue;
+                    }
+
+                    //Read rows below header
+                    var line = reader.ReadLine();
+                    string[] key_value = BasicHelpers._SplitCSV(line);//line.Split(',');
+
+                    //Check if iso3166 1 alpha 2 matches
+                    if (key_value[0] == languageCode)
+                    {
+                        var kvp = new KeyValuePair<string, string>(BasicHelpers._TrimQuotesFromString(key_value[0]), BasicHelpers._TrimQuotesFromString(key_value[1]));
+                        return kvp;
+                    }
+
+                }
+            }
+
+            throw new Exception($@"ZR in GetValueBy_LanguageCode(string languageCode): could not find languageCode with key of {languageCode}");
+
+        }
+
+    }
+
+    public class Reader_GitHubUser_Umpirsky_Locale
+    {
+        private string _FilePath_dataFolder { get; set; }
+        private string _LocaleFolder { get; set; }
+
+        private string _GetFileDataFrom_csv { get { return $@"{this._FilePath_dataFolder}{this._LocaleFolder}\locales.csv"; } }
+
+        //**********************
+        //CONSTRUCTOR
+        //**********************
+        public Reader_GitHubUser_Umpirsky_Locale(string filePath_dataFolder, string localeFolder)
+        {
+            this._FilePath_dataFolder = filePath_dataFolder;
+            this._LocaleFolder = localeFolder;
+        }
+
+        public void SetLocalFolder(string localFolderName)
+        {
+            this._LocaleFolder = localFolderName;
+        }
+
+        public Dictionary<string, string> GetLocaleDataAll()
+        {
+            Dictionary<string, string> keyValuePairData = new Dictionary<string, string>();
+
+            using (var reader = new StreamReader(this._GetFileDataFrom_csv))
+            {
+
+                bool isThisFirstHeaderRow = true;
+
+                while (!reader.EndOfStream)
+                {
+                    //Read header
+                    if (isThisFirstHeaderRow)
+                    {
+                        var line_header = reader.ReadLine();
+                        isThisFirstHeaderRow = false;
+                        continue;
+                    }
+
+                    //Read rows below header
+                    var line = reader.ReadLine();
+                    string[] key_value = BasicHelpers._SplitCSV(line);//line.Split(',');
+
+                    //Save Header as key and row value as value in dictionary
+                    keyValuePairData.Add(BasicHelpers._TrimQuotesFromString(key_value[0]), BasicHelpers._TrimQuotesFromString(key_value[1]));
+
+                }
+            }
+
+            return keyValuePairData;
+        }
+
+        public KeyValuePair<string, string> GetValueBy_LocaleCode(string languageCode)
+        {
+            using (var reader = new StreamReader(this._GetFileDataFrom_csv))
+            {
+
+                bool isThisFirstHeaderRow = true;
+
+                while (!reader.EndOfStream)
+                {
+                    //Read header
+                    if (isThisFirstHeaderRow)
+                    {
+                        isThisFirstHeaderRow = false;
+                        continue;
+                    }
+
+                    //Read rows below header
+                    var line = reader.ReadLine();
+                    string[] key_value = BasicHelpers._SplitCSV(line);//line.Split(',');
+
+                    //Check if iso3166 1 alpha 2 matches
+                    if (key_value[0] == languageCode)
+                    {
+                        var kvp = new KeyValuePair<string, string>(BasicHelpers._TrimQuotesFromString(key_value[0]), BasicHelpers._TrimQuotesFromString(key_value[1]));
+                        return kvp;
+                    }
+
+                }
+            }
+
+            throw new Exception($@"ZR in GetValueBy_LocaleCode(string languageCode): could not find languageCode with key of {languageCode}");
+
+        }
+
+    }
+
+
+
+
+
+    internal class BasicHelpers
+    {
+        internal static void CheckIfFolderPathExits(string folderPath)
+        {
+            bool exits = Directory.Exists(folderPath);
+
+            if (!exits)
+                throw new Exception($"ZR the folder path {folderPath} does not exist");
+        }
+
+        internal static string[] _SplitCSV(string input)
         {
             //Check this stackoverflow post on how to parse csv file when escaping commas in a quotation:
             //--->   dat,"ok,hello,yes",end, end2
@@ -138,66 +486,16 @@ namespace CountryDataForGis
             return list.ToArray();
         }
 
-
-
-
-        //public Dictionary<string, string>[][] GetAllData()
-        //{
-        //    //List<Dictionary<string, string>> listResultDictionary = new List<Dictionary<string, string>>();
-
-        //    using (var reader = new StreamReader(this._FilePath_csv_countryCodes))
-        //    {
-
-        //        bool isThisFirstHeaderRow = true;
-        //        string[] arrHeaders = new string[] { };
-
-        //        while (!reader.EndOfStream)
-        //        {
-        //            //Read header
-        //            if (isThisFirstHeaderRow)
-        //            {
-        //                var lineHeader = reader.ReadLine();
-        //                arrHeaders = lineHeader.Split(',');
-        //                isThisFirstHeaderRow = false;
-        //                continue;
-        //            }
-
-        //            //Read rows below header
-        //            var line = reader.ReadLine();
-        //            string[] values = line.Split(',');
-
-        //            //Save Header as key and row value as value in dictionary
-        //            for (int i = 0; i < values.Length; i++)
-        //            {
-        //                listResultDictionary.Add();
-        //            }
-        //        }
-        //    }
-
-        //    return listResultDictionary;
-
-        //}
-
-
-
-    }
-
-
-
-
-
-    internal class BasicHelpers
-    {
-        internal static void CheckIfFolderPathExits(string folderPath)
+        internal static string _TrimQuotesFromString(string stringToTrim)
         {
-            bool exits = Directory.Exists(folderPath);
+            //will remove " quotes sigle character from the beginning and end of the string
+            //Check stackoverflow post
+            //https://stackoverflow.com/questions/2639918/how-to-remove-char-from-the-begin-and-the-end-of-a-string
+            stringToTrim = Regex.Replace(stringToTrim, "^\"|\"$", "");
 
-            if (!exits)
-                throw new Exception($"ZR the folder path {folderPath} does not exist");
+            return stringToTrim;
         }
 
-
-     
 
     }
 
